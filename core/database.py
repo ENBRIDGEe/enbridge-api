c3468e729d8d4ba7ad977c3973f19d150888c0fe
sampraad <sampraaddas2503@gmail.com>
2026-05-17 23:39:59 +0530

    prod ready. engine uses querypool instead of nullpool to make requests faster

--- core/database.py
+++ core/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from sqlalchemy.pool import NullPool
+from sqlalchemy.pool import QueuePool
 from dotenv import load_dotenv
 import os
 
@@ -17,8 +17,16 @@ DBNAME = os.getenv("dbname")
 # Construct the SQLAlchemy connection string
 DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
 
-# Create the SQLAlchemy engine
-engine = create_engine(DATABASE_URL, poolclass=NullPool)
+# Create the SQLAlchemy engine with QueuePool for connection reuse
+# pool_size: number of connections to keep in pool; max_overflow: additional connections when pool is exhausted
+engine = create_engine(
+    DATABASE_URL,
+    poolclass=QueuePool,
+    pool_size=10,
+    max_overflow=20,
+    pool_pre_ping=True,  # Verify connections are alive before using
+    pool_recycle=3600    # Recycle connections after 1 hour to prevent timeout
+)
 # If using Transaction Pooler or Session Pooler, we want to ensure we disable SQLAlchemy client side pooling -
 # https://docs.sqlalchemy.org/en/20/core/pooling.html#switching-pool-implementations
 
